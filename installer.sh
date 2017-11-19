23697962bd04b746df6dddd9e3fc178aa680d521
Roger Miret <rogerxaic@users.noreply.github.com>
2017-11-19 22:10:17 +0100

    bashrc basics

--- installer.sh
+++ installer.sh
@@ -8,6 +8,8 @@ sudo apt install -y curl wget screen build-essential htop openssh-server git unz
 curl https://raw.githubusercontent.com/rogerxaic/starter/master/.bashrc >> ~/.bashrc
 source ~/.bashrc
 
+sudo apt install -y localepurge deborphan
+
 #install nodejs
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt install -y nodejs
