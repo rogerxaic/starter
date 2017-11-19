287c35ec9980cbd4b4dc3befe3e3e06a423612c3
Roger Miret <rogerxaic@users.noreply.github.com>
2017-11-19 18:15:43 +0100

    added bashrc

--- installer.sh
+++ installer.sh
@@ -5,6 +5,9 @@ sudo apt upgrade
 #install basics
 sudo apt install -y curl wget screen build-essential htop openssh-server git
 
+curl https://raw.githubusercontent.com/rogerxaic/starter/master/.bashrc >> ~/.bashrh
+source ~/.bashrc
+
 #install nodejs
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt install -y nodejs
