92b09c706b07bbfed629bea606f530fda7c716cf
Roger Miret <rogerxaic@users.noreply.github.com>
2017-11-19 18:02:54 +0100

    added gti

--- installer.sh
+++ installer.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt upgrade 
 
 #install basics
-sudo apt install -y curl wget screen build-essential htop
+sudo apt install -y curl wget screen build-essential htop openssh-server git
 
 #install nodejs
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
